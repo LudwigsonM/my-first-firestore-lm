898405204a5e0b37f04b44c1f3999fdb4ec5ec57
Ludwigson Morisseau <morisseau@Ludwigsons-MacBook-Air.local>
2022-04-29 11:17:00 -0400

    created first restaurant

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("path/to/serviceAccountKey.json");
+const serviceAccount = require('./credentials.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
+const db = admin.firestore();
+
+const restaurant = {
+    name: 'Mister 01',
+    address: '555 Federal Hwy',
+    cuisine: 'Pizza',
+    rating: 4.9,
+    phone: '(786) 677-2903'
+}
+
+db.collection('restaurants').add(restaurant)
+    .then(doc => console.log('Created restaurant', doc.id))
+    .catch(err => console.error(err))
